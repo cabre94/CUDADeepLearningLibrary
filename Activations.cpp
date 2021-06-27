63c9c76e267ba8d17937dfe700a7c6375cdbab4b
cabre94 <cabre94@hotmail.com>
2021-06-27 18:06:10 -0300

    Add file

---
+++ Activations.cpp
@@ -0,0 +1,18 @@
+/*
+date: 27-06-21
+File: Activations.cpp
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+*/
+
+#include <iostream>
+#include <string>
+
+int main(int argc, const char** argv) {
+
+
+    return 0;
+}
