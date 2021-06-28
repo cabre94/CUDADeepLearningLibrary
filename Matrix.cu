60a181edbb343cd7b995b772737a5b8d5ed6b75b
Facundo Martín Cabrera <facundo.cabrera@ib.edu.ar>
2021-06-28 15:34:23 -0300

    Change extension and add distribution initialization

--- Matrix.cu
+++ Matrix.cu
@@ -4,18 +4,18 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <random>
 
-__global__ void hello(){
-    printf("Hola\n");
-}
 
 class Matrix{
 public:
 	int height, width, size;
 	float *h_elem, *d_elem;
+	float weight;
+	std::string dist;
 
 // public:
-	Matrix(int width, int height);
+	Matrix(int width, int height, std::string dist_ = "uniform", float w = 1);
 	~Matrix();
 
 	void copyDeviceToHost();
@@ -26,12 +26,24 @@ public:
 };
 
 
-Matrix::Matrix(int height, int width) : height(height), width(width), size(width * height){
+Matrix::Matrix(int height, int width, std::string dist_, float w)
+			: height(height), width(width), size(width * height){
+	weight = w;
+	dist = dist_;
 	h_elem = new float[size];
-	// float aux[3] = {-1, 0 , 1};
-	for(size_t i=0; i < size; ++i){
-		h_elem[i] = i;
-		// h_elem[i] = aux[i%3];
+	
+	// float aux[3] = {-2, 0 , 2};
+	// for(int i=0; i < size; ++i){
+	// 	// h_elem[i] = i;
+	// 	// h_elem[i] = 0;
+	// 	h_elem[i] = aux[i%3];
+	// }
+	if(dist == "uniform"){
+		std::default_random_engine generator;
+  		std::normal_distribution<float> distribution(0.0,weight);
+		for(int i=0; i < size; ++i){
+			h_elem[i] = distribution(generator);
+		}
 	}
 
 	// Allocacion en device
