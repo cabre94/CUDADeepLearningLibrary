6c9ccbac2503fbf8525c09a4a1cd66e943b11bcd
Facundo Martín Cabrera <facundo.cabrera@ib.edu.ar>
2021-06-29 01:40:27 -0300

    add templete of optimizer class

--- optimizers.cu
+++ optimizers.cu
@@ -7,6 +7,32 @@
 #include "Matrix.cu"
 
 
+/* ----------------------------
+Optimizer class
+---------------------------- */
+class Optimizer{
+private:
+    float lr;
+    
+public:
+	Optimizer(float lr);	//Default constructor
+	virtual ~Optimizer();
+	
+	// std::string getName();
+	// virtual void call(Matrix &in, Matrix &out) = 0;
+	// virtual void gradient(Matrix &in, Matrix &out) = 0;
+	// virtual void printWeights() = 0;
+	// virtual int getWidth() = 0;
+	// virtual int getHeight() = 0;
+	// virtual std::string getActivation() = 0;
+
+
+	
+};
+
+Optimizer::Optimizer(float lr) : lr(lr) {}
+
+Optimizer::~Optimizer(){}
 
 
 
