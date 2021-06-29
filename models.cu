2e91b451aeee5d079810bc1a0f1d1d57db2087c5
Facundo Martín Cabrera <facundo.cabrera@ib.edu.ar>
2021-06-28 23:29:02 -0300

    add NeuralNetwork class

--- models.cu
+++ models.cu
@@ -5,6 +5,46 @@
 #include <string>
 #include <stdio.h>
 #include "Matrix.cu"
+#include "Activation.cu"
+#include "layers.cu"
+#include "losses.cu"
+#include "optimizers.cu"
+#include "metrics.cu"
+
+
+class NeuralNetwork{
+private:
+    std::vector<Layer*> layers;
+    
+public:
+	NeuralNetwork();	//Default constructor
+	~NeuralNetwork();
+	
+	// std::string getName();
+	// virtual void call(Matrix &in, Matrix &out) = 0;
+	// virtual void gradient(Matrix &in, Matrix &out) = 0;
+	void add(Layer *layer);
+	void print();
+};
+
+NeuralNetwork::NeuralNetwork(){}
+
+NeuralNetwork::~NeuralNetwork(){
+	std::vector<Layer*>::iterator itr;
+	for(itr = layers.begin(); itr != layers.end(); ++itr){
+		delete (*itr);
+	}
+}
+
+void NeuralNetwork::print(){
+	std::vector< int* >::iterator itr;
+	for(itr = layers.begin(); itr != layers.end(); ++itr){
+		(*itr)->getName();
+	}
+}
+
+
+
 
 
 
