c6c4a8371118201a2e11a8443a8ea9ac717338d8
Facundo Martín Cabrera <facundo.cabrera@ib.edu.ar>
2021-07-02 00:58:00 -0300

    add SGD class

--- optimizers.cu
+++ optimizers.cu
@@ -9,7 +9,7 @@
 #include <ctime>
 #include <cstdlib>      // std::rand, std::srand
 #include "Matrix.cu"
-#include "models.cu"
+// #include "models.cu"
 #include "layers.cu"
 
 class NeuralNetwork;
@@ -20,31 +20,59 @@ Optimizer class
 class Optimizer{
 private:
 	std::string name;
-    float lr;
 
 	float cost;
 	float acc;
     
 public:
-	Optimizer(float lr);	//Default constructor
+	Optimizer();	//Default constructor
 	virtual ~Optimizer();
 
-	void call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs);
+	void call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs) = 0;
 
-	void updateW(Layer *layer);
+	void updateW(Layer *layer) = 0;
 
 	float getCost();
 	float getAcc();
 	
 };
 
-Optimizer::Optimizer(float lr) : lr(lr) {}
+Optimizer::Optimizer(std::string name_) : name(name_){}
 
 Optimizer::~Optimizer(){}
 
-void Optimizer::call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs){
-	int nSamples = X.getHeight();
-	// int nBatch = int(nSamples / bs);
+float Optimizer::getCost(){return cost;}
+
+float Optimizer::getAcc(){return acc;}
+
+
+
+/* ----------------------------
+SGD class
+---------------------------- */
+class SGD : public Optimizer{
+private:
+    float lr;
+	
+public:
+	SGD(float lr);	//Default constructor
+	virtual ~SGD();
+	
+	void call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs);
+	
+	void updateW(Layer *layer);
+	
+};
+
+SGD::SGD(float lr) : Optimizer("SGD"), lr(lr) {}
+
+SGD::~SGD(){}
+
+void SGD::call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs){
+	int nSamples = X.getHeight();		// # de datos
+	int nBatch = int(nSamples / bs);	// # de batchs
+	float loss_mean = 0;				// Loss de la epoca
+	
 	for(int from = 0, to = bs-1; to < nSamples; from+=bs, to+=bs){
 	// for(int from = 0, to = nBatch-1; to < nSamples; from+=bs, to+=bs){
 		// Copiar a la matrix del primer layer los datos
@@ -52,17 +80,24 @@ void Optimizer::call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs
 		// itr = layers.begin();
 		itr = NN.getLayers().begin();
 		(*itr)->getOutput().copyDeviceDataFromBatch(X, d_idx, from);
-
+		
 		NN.forward();
+		
+		loss_mean += NN.getLoss()->call();
+		// Calcular metrica
+		
+		//Backward
+		
+		// Actualizar W
+		// Actualizar b
 	}
+	NN.getLoss().push_back(loss_mean/nBatch);
 }
-
-void Optimizer::updateW(Layer *layer){
+	
+void SGD::updateW(Layer *layer){
 	std::cout << "Falta implementar" << std::endl;
 }
-
-float Optimizer::getCost(){return cost;}
-
-float Optimizer::getAcc(){return acc;}
-
+	
+	
 #endif
+	
