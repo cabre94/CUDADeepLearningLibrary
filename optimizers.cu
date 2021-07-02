7ec86bda67032d280db70e30744345bde2fd3015
Facundo Martín Cabrera <facundo.cabrera@ib.edu.ar>
2021-07-01 22:42:12 -0300

    start to make call method

--- optimizers.cu
+++ optimizers.cu
@@ -4,29 +4,37 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <algorithm>    // std::random_shuffle
+#include <vector>       // std::vector
+#include <ctime>
+#include <cstdlib>      // std::rand, std::srand
 #include "Matrix.cu"
+#include "models.cu"
+#include "layers.cu"
 
-
+class NeuralNetwork;
+class Layer;
 /* ----------------------------
 Optimizer class
 ---------------------------- */
 class Optimizer{
 private:
+	std::string name;
     float lr;
+
+	float cost;
+	float acc;
     
 public:
 	Optimizer(float lr);	//Default constructor
 	virtual ~Optimizer();
-	
-	// std::string getName();
-	// virtual void call(Matrix &in, Matrix &out) = 0;
-	// virtual void gradient(Matrix &in, Matrix &out) = 0;
-	// virtual void printWeights() = 0;
-	// virtual int getWidth() = 0;
-	// virtual int getHeight() = 0;
-	// virtual std::string getActivation() = 0;
 
+	void call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs);
 
+	void updateW(Layer *layer);
+
+	float getCost();
+	float getAcc();
 	
 };
 
@@ -34,7 +42,27 @@ Optimizer::Optimizer(float lr) : lr(lr) {}
 
 Optimizer::~Optimizer(){}
 
+void Optimizer::call(Matrix &X, Matrix &Y, NeuralNetwork &NN, int *d_idx, int bs){
+	int nSamples = X.getHeight();
+	// int nBatch = int(nSamples / bs);
+	for(int from = 0, to = bs-1; to < nSamples; from+=bs, to+=bs){
+	// for(int from = 0, to = nBatch-1; to < nSamples; from+=bs, to+=bs){
+		// Copiar a la matrix del primer layer los datos
+		std::vector<Layer*>::iterator itr;
+		// itr = layers.begin();
+		itr = NN.getLayers().begin();
+		(*itr)->getOutput().copyDeviceDataFromBatch(X, d_idx, from);
 
+		NN.forward();
+	}
+}
 
+void Optimizer::updateW(Layer *layer){
+	std::cout << "Falta implementar" << std::endl;
+}
+
+float Optimizer::getCost(){return cost;}
+
+float Optimizer::getAcc(){return acc;}
 
 #endif
